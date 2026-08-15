d2c7e37f7d9160dceb5c85cc0862a0464a9cda88
caiofeitosaa <caiorcfeitosa@gmail.com>
2026-08-15 00:51:56 -0300

    first commit of my calculator app without using AI

---
+++ src/main/java/org/example/calculator/CalculatorApplication.java
@@ -0,0 +1,19 @@
+package org.example.calculator;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class CalculatorApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(CalculatorApplication.class.getResource("Calculator.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Calculator");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
---
+++ src/main/java/org/example/calculator/Launcher.java
@@ -0,0 +1,9 @@
+package org.example.calculator;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(CalculatorApplication.class, args);
+    }
+}
